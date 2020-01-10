3b78d4b8ebdb2bd805193f145487542d756108fb
MPkekava <54512031+MPkekava@users.noreply.github.com>
2020-01-10 14:08:11 +0200

    teksts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,json,jsonify
 
 app = Flask('app')
 
@@ -8,6 +8,14 @@ def index_page():
 @app.route('/health')
 def helth_check():
   return "OK"
+  @app.route('/chats/lasi')
+  def ielasit_chatu();
+    chata_rindas=[]
+    with open("chats.txt","r")as f:
+      for rinda in f:
+          chata_rindas.append(rinda)
+
+        return jsonify({"chats":chata_rindas})
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
