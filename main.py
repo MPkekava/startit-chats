0daa42dec946e7c8dfc483f397d9c27575bc37a1
MPkekava <54512031+MPkekava@users.noreply.github.com>
2020-01-10 12:27:13 +0200

    icona

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
-
-
+ffrom flask import Flask, render_template
 app = Flask('app')
-
-
-@app.route('/health')
-def health_check():
+@app.route('/')
+def index_page():
+  return render_template("index.html")
+@app.route('/helth')
+def helth():
   return "OK"
-
-
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
